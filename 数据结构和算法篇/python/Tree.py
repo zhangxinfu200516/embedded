cfcc6df14ffeabd5d5534dd8990f3c2c81c19dbb
zxf <zhang_200516@qq.com>
2026-01-26 21:22:25 +0800

    push-树-层序遍历

--- 数据结构和算法篇/python/Tree.py
+++ 数据结构和算法篇/python/Tree.py
@@ -31,5 +31,27 @@ node2.right = node5
 node3.right = node6
 
 #print(root)
-preorder_traversal(root)
-preorder_traversal(node2)
+#preorder_traversal(root)
+#preorder_traversal(node2)
+
+from collections import deque
+
+def level_order_traversal(root):
+    """层序遍历：使用队列"""
+    if root is None:
+        return
+
+    queue = deque([root]) # 初始化队列，放入根节点
+
+    while queue:
+        current_node = queue.popleft() # 1. 从队列左侧取出节点
+        print(current_node.value, end=' ') # 2. 访问该节点
+
+        # 3. 将该节点的子节点（如果存在）按顺序加入队列右侧
+        if current_node.left:
+            queue.append(current_node.left)
+        if current_node.right:
+            queue.append(current_node.right)
+
+print("\n层序遍历结果:", end=' ')
+level_order_traversal(root) # 输出: 1 2 3 4 5 6
