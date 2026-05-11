a252b1952e51070fe71b3bdc84d2789c12857a73
zxf <zhang_200516@qq.com>
2026-05-11 12:08:39 +0800

    push-dp真题（D路通讯：最长字串/字迹、奖品兑换组合：多重背包+每种商品处理一次：3 5 / 5 3不成立 4 4 可以、地图穿越：二维dp、难在初始化为INT_MAX，及条件处理）

--- C++/dp/dp2.cpp
+++ C++/dp/dp2.cpp
@@ -153,7 +153,7 @@ int main04()
 	cout << dp[n];
 }
 // 多重背包
-int main()
+int main05()
 {
 	int bagWeight, n;
 	cin >> bagWeight >> n;
@@ -186,9 +186,9 @@ int main()
 	}
 	cout << "--------------";
 }
-class Solution
+class Solution3
 {
-public: 
+public:
 	int coinChange(vector<int> &coins, int amount)
 	{
 		vector<int> dp(amount + 1, INT_MAX / 2);
@@ -209,3 +209,143 @@ public:
 			return dp[amount];
 	}
 };
+// 最长重复子串
+class Solution4
+{
+public:
+	int findLength(vector<int> &nums1, vector<int> &nums2)
+	{
+		int n1 = nums1.size();
+		int n2 = nums2.size();
+		vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0)); // 以s1的第几个字符、s2的第几个字符结尾 的最长字符串的长度
+		int max_lenght = -INT_MAX;
+		for (int i = 1; i < n1 + 1; i++)
+		{
+			for (int j = 1; j < n2 + 1; j++)
+			{
+				if (nums1[i - 1] == nums2[j - 1])
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				max_lenght = max(dp[i][j], max_lenght);
+			}
+		}
+		return max_lenght;
+	}
+};
+// 多重背包（组合数：将等价值/重量的商品视为同一商品，此时需要考虑每种只处理一次（可以取多个））- 奖品兑换
+int main06()
+{
+	int n;
+	cin >> n;
+	vector<int> values(n, 0);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> values[i];
+	}
+	int target;
+	cin >> target;
+	// 统计每种价值的数量
+	unordered_map<int, int> freq;
+	for (int val : values)
+	{
+		freq[val]++;
+	}
+	vector<int> dp(target + 1, 0);
+	dp[0] = 1;
+	for (auto &p : freq)
+		cout << p.first << " ";
+	cout << endl;
+	for (auto &p : freq)
+		cout << p.second << " ";
+	cout << endl;
+	for (auto &p : freq)
+	{
+		int val = p.first;	// 奖品价值
+		int cnt = p.second; // 该奖品的个数
+		for (int j = target; j >= 0; j--)
+		{
+			// 最后枚举选择该奖品的个数（1 ~ cnt）
+			for (int k = 1; k <= cnt && j >= k * val; ++k)
+			{
+				dp[j] += dp[j - k * val];
+			}
+			// dp[j] = dp[j] + dp[j - val];
+		}
+		for (int j = 0; j < target + 1; j++)
+			cout << dp[j] << " ";
+		cout << endl;
+	}
+	cout << dp[target];
+}
+int main()
+{
+	int k;
+	cin >> k;
+	vector<vector<int>> cost(k, vector<int>(k, 0));
+	for (int i = 0; i < k; i++)
+	{
+		for (int j = 0; j < k; j++)
+			cin >> cost[i][j];
+	}
+	vector<vector<int>> dp(k, vector<int>(k, INT_MAX));
+	// 初始化
+	// dp[0][0] = cost[0][0];
+	// for (int i = 1; i < k; i++)
+	// {
+	// 	if (cost[0][i] - cost[0][i - 1] <= 1)
+	// 		dp[0][i] = dp[0][i - 1] + cost[0][i];
+	// 	else
+	// 		break;
+	// }
+	// for (int i = 1; i < k; i++)
+	// {
+	// 	if (cost[i][0] - cost[i - 1][0] <= 1)
+	// 		dp[i][0] = dp[i - 1][0] + cost[i][0];
+	// 	else
+	// 		break;
+	// }
+	// for (int i = 1; i < k; i++)
+	// {
+	// 	for (int j = 1; j < k; j++)
+	// 	{
+	// 		int min_cost = INT_MAX;
+	// 		if (dp[i - 1][j] != INT_MAX && abs(cost[i][j] - cost[i - 1][j]) <= 1)
+	// 			min_cost = min(dp[i - 1][j] + cost[i][j], min_cost);
+	// 		if (dp[i][j - 1] != INT_MAX && abs(cost[i][j] - cost[i][j - 1]) <= 1)
+	// 			min_cost = min(dp[i][j - 1] + cost[i][j], min_cost);
+
+	// 		dp[i][j] = min_cost;
+	// 	}
+	// }
+	dp[0][0] = cost[0][0];
+	for (int i = 0; i < k; i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			if (i == 0 && j == 0)
+				continue;
+			if (i > 0 && dp[i - 1][j] != INT_MAX && abs(cost[i][j] - cost[i - 1][j]) < 2)
+				dp[i][j] = min(dp[i][j], dp[i - 1][j] + cost[i][j]);
+			if (j > 0 && dp[i][j - 1] != INT_MAX && abs(cost[i][j] - cost[i][j - 1]) < 2)
+				dp[i][j] = min(dp[i][j], dp[i][j - 1] + cost[i][j]);
+		}
+	}
+	// 打印测试
+	for (int i = 0; i < k; i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			cout << dp[i][j] << " ";
+		}
+		cout << endl;
+	}
+	// 取最右边的最小值
+	int min_dp = INT_MAX;
+	for (int i = 0; i < k; i++)
+	{
+		min_dp = min(min_dp, dp[i][k - 1]);
+	}
+	if (min_dp == INT_MAX)
+		cout << -1;
+	else
+		cout << min_dp;
+}
