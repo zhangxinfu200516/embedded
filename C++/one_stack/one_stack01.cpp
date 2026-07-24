f446b8fad9f2d48c20813d7b305bfb4daac2a8a7
zxf <zhang_200516@qq.com>
2026-07-25 00:03:34 +0800

    push-单调栈题型：下一个更大元素 II

--- C++/one_stack/one_stack01.cpp
+++ C++/one_stack/one_stack01.cpp
@@ -24,7 +24,7 @@ public:
 		return temperatures;
 	}
 };
-class Solution
+class Solution02
 {
 public:
 	vector<int> dailyTemperatures(vector<int> &temperatures)
@@ -44,24 +44,24 @@ public:
 		return result;
 	}
 };
-class Solution
+class Solution03
 {
 public:
 	vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
 	{
-		vector<int> result(nums1.size(),-1);
-		for(int i = 0; i < nums1.size() ; i++)
+		vector<int> result(nums1.size(), -1);
+		for (int i = 0; i < nums1.size(); i++)
 		{
 			static bool access = false;
 
-			for(int j = 0; j < nums2.size(); j++)
+			for (int j = 0; j < nums2.size(); j++)
 			{
-				if(nums1[i] == nums2[j])
+				if (nums1[i] == nums2[j])
 					access = true;
 
-				if(access)
+				if (access)
 				{
-					if(nums1[i] < nums2[j])
+					if (nums1[i] < nums2[j])
 					{
 						result[i] = nums2[j];
 						access = false;
@@ -73,6 +73,40 @@ public:
 		return result;
 	}
 };
+class Solution04
+{
+public:
+	vector<int> nextGreaterElements(vector<int> &nums)
+	{
+		vector<int> result(nums.size(), -1);
+		int n = nums.size();
+		for(int i = 0; i < n; i++)
+		{
+			nums.push_back(nums[i]);
+		}
+		for (int j = 0; j < result.size(); j++)
+		{
+
+			for (int k = j + 1; k < nums.size(); k++)
+			{
+				if (nums[j] < nums[k])
+				{
+					result[j] = nums[k];
+					break;
+				}
+			}
+		}
+		return result;
+	}
+};
 int main()
 {
+	Solution04 s;
+	vector<int> nums = {1,2,3,4,3};
+	vector<int> result = s.nextGreaterElements(nums);
+	for (int i = 0; i < result.size(); i++)
+	{
+		cout << result[i] << " ";
+	}
+	return 0;
 }
