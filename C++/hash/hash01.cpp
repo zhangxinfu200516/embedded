cc387dc3dd91bef613a5434e3db9ca848bc20c18
zxf <zhang_200516@qq.com>
2026-08-01 20:40:14 +0800

    push-哈希题型：快乐数、两数之和、四数相加 II、赎金信

--- C++/hash/hash01.cpp
+++ C++/hash/hash01.cpp
@@ -20,7 +20,7 @@ public:
 		return true;
 	}
 };
-class Solution
+class Solution02
 {
 public:
 	vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
@@ -40,9 +40,102 @@ public:
 		return result;
 	}
 };
+class Solution03
+{
+public:
+	bool isHappy(int n)
+	{
+		vector<int> record;
+		int val = n;
+		std::unordered_set<int> set;
+		while (true)
+		{
+			int sum = 0;
+			while (val > 0)
+			{
+				sum += (val % 10) * (val % 10);
+				val = val / 10;
+			}
+			if (sum == 1)
+				return true;
+			if (set.count(sum) > 0)
+				return false;
+			set.insert(sum);
+			val = sum;
+		}
+	}
+};
+class Solution04
+{
+public:
+	vector<int> twoSum(vector<int> &nums, int target)
+	{
+		for (int i = 0; i < nums.size() - 1; i++)
+		{
+			for (int j = i + 1; j < nums.size(); j++)
+			{
+				if (nums[i] + nums[j] == target)
+					return {i, j};
+			}
+		}
+		return {};
+	}
+};
+class Solution05
+{
+public:
+	int fourSumCount(vector<int> &nums1, vector<int> &nums2, vector<int> &nums3, vector<int> &nums4)
+	{
+		int result = 0;
+		for (int i = 0; i < nums1.size(); i++)
+		{
+			for (int j = 0; j < nums2.size(); j++)
+			{
+				for (int m = 0; m < nums3.size(); m++)
+				{
+					for (int n = 0; n < nums4.size(); n++)
+					{
+						if (nums1[i] + nums2[j] + nums3[m] + nums4[n] == 0)
+						{
+							result++;
+						}
+					}
+				}
+			}
+		}
+		return result;
+	}
+};
+class Solution
+{
+public:
+	bool canConstruct(string ransomNote, string magazine)
+	{
+		std::unordered_map<char, int> map;
+		for (int i = 0; i < magazine.size(); i++)
+			map[magazine[i]]++;
+		for (int j = 0; j < ransomNote.size(); j++)
+		{
+			map[ransomNote[j]]--;
+			if (map[ransomNote[j]] < 0)
+				return false;
+		}
+		return true;
+	}
+};
 int main()
 {
-	std::unordered_map<int, int> map;
-	cout << map[0] << endl;
+	// Solution s;
+
+	// cout << s.isHappy(19) << endl;
+	// std::unordered_set<int> set;
+	// set.insert(1);
+	// set.insert(2);
+
+	// cout << set.count(3) << endl;
+	std::unordered_map<char, int> map;
+	cout << map['s'] << endl;
+	map['s']--;
+	cout << map['s'] << endl;
 	return 0;
 }
