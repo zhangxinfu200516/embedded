5658beab631f408ff205eb5d12db7dcc26530b0c
zxf <zhang_200516@qq.com>
2026-08-12 22:52:56 +0800

    push-dp题型：不同路径、不同路径 II、不同的二叉搜索树

--- C++/dp/dp01.cpp
+++ C++/dp/dp01.cpp
@@ -13,7 +13,7 @@ public:
 		return dp[n];
 	}
 };
-class Solution
+class Solution02
 {
 public:
 	int climbStairs(int n)
@@ -26,7 +26,7 @@ public:
 		return dp[n];
 	}
 };
-class Solution
+class Solution03
 {
 public:
 	int minCostClimbingStairs(vector<int> &cost)
@@ -37,6 +37,64 @@ public:
 		for (int i = 2; i < n; i++)
 			dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
 		return dp[n - 1];
+	}
+};
+class Solution04
+{
+public:
+	int uniquePaths(int m, int n)
+	{
+		vector<vector<int>> dp(m, vector<int>(n, 0));
+		for (int i = 0; i < m; i++)
+			dp[i][0] = 1;
+		for (int j = 0; j < n; j++)
+			dp[0][j] = 1;
+		for (int i = 1; i < m; i++)
+		{
+			for (int j = 1; j < n; j++)
+			{
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+			}
+		}
+		return dp[m - 1][n - 1];
+	}
+};
+class Solution05
+{
+public:
+	int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
+	{
+		int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+		vector<vector<int>> dp(m, vector<int>(n, 0));
+		for (int i = 0; i < m; i++)
+			dp[i][0] = 1 * (!obstacleGrid[i][0]);
+		for (int j = 0; j < n; j++)
+			dp[0][j] = 1 * (!obstacleGrid[0][j]);
+		for (int i = 1; i < m; i++)
+		{
+			for (int j = 1; j < n; j++)
+			{
+				dp[i][j] = dp[i - 1][j] * (!obstacleGrid[i][j]) + dp[i][j - 1] * (!obstacleGrid[i][j]);
+			}
+		}
+		return dp[m - 1][n - 1];
+	}
+};
+class Solution
+{
+public:
+	int numTrees(int n)
+	{
+		vector<int> dp(n + 1, 0);
+		dp[0] = 1;
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				dp[i] += dp[j] * dp[i - 1 - j];
+			}
+		}
+		return dp[n];
 	}
 };
 int main()
