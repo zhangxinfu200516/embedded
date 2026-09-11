9481fceb1a2d1da8a49328b350ffa927e58ca6bb
zxf <zhang_200516@qq.com>
2026-09-11 22:16:46 +0800

    push- Hot100 1道编程题

--- C++/hot100/test1.cpp
+++ C++/hot100/test1.cpp
@@ -394,6 +394,27 @@ public:
 		return result;
 	}
 };
+class Solution
+{
+
+public:
+	static bool func(const vector<int> &a, const vector<int> &b)
+	{
+		if (a[0] == b[0])
+			return a[1] < b[1];
+		return a[0] > b[0];
+	}
+	vector<vector<int>> reconstructQueue(vector<vector<int>> &people)
+	{
+		sort(people.begin(), people.end(), func);
+		vector<vector<int>> result;
+		for (int i = 0; i < people.size(); i++)
+		{
+			result.insert(result.begin() + people[i][1], people[i]);
+		}
+		return result;
+	}
+};
 int main()
 {
 }
