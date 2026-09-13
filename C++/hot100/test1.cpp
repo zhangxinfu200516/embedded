8f5720ba922e5fb7580a5675049ad8e1b64e2cf5
zxf <zhang_200516@qq.com>
2026-09-13 21:26:31 +0800

    push- Hot100 3道题

--- C++/hot100/test1.cpp
+++ C++/hot100/test1.cpp
@@ -542,6 +542,56 @@ public:
 		return max(it.first, it.second);
 	}
 };
+class Solution20
+{
+public:
+	int maxProfit(vector<int> &prices)
+	{
+		vector<vector<int>> dp(prices.size(), vector<int>(2, 0));
+		dp[0][0] = -prices[0];
+		for (int i = 1; i < prices.size(); i++)
+		{
+			dp[i][0] = max(dp[i - 1][0], -prices[i]);
+			dp[i][1] = max(dp[i - 1][1], dp[i][0] + prices[i]);
+		}
+		return dp[prices.size() - 1][1];
+	}
+};
+class Solution21
+{
+public:
+	int maxProfit(vector<int> &prices)
+	{
+		vector<vector<int>> dp(prices.size(), vector<int>(3, 0));
+		dp[0][0] = -prices[0];
+		for (int i = 1; i < prices.size(); i++)
+		{
+			dp[i][0] = max(dp[i - 1][0], max(dp[i - 1][0], dp[i - 1][3]) - prices[i]);
+			dp[i][1] = max(dp[i - 1][1], dp[i - 1][3]);
+			dp[i][2] = dp[i][0] + prices[i];
+			dp[i][3] = dp[i - 1][2];
+		}
+		return max({dp[prices.size() - 1][0], dp[prices.size() - 1][1], dp[prices.size() - 1][2], dp[prices.size() - 1][3]});
+	}
+};
+class Solution22
+{
+public:
+	int lengthOfLIS(vector<int> &nums)
+	{
+		vector<int> dp(nums.size(), 1);
+		for (int i = 1; i < nums.size(); i++)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				if (nums[i] > nums[j])
+					dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		sort(dp.begin(), dp.end());
+		return dp[nums.size() - 1];
+	}
+};
 int main()
 {
 }
