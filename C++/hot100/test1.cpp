ee3bf8d0ab5da80a66470a0dca7d20e99b63160d
zxf <zhang_200516@qq.com>
2026-09-24 23:52:21 +0800

    push-Hot100-和为k的子数组

--- C++/hot100/test1.cpp
+++ C++/hot100/test1.cpp
@@ -990,6 +990,24 @@ public:
 		return result;
 	}
 };
+class Solution36
+{
+public:
+	int subarraySum(vector<int> &nums, int k)
+	{
+		int result = 0, sum = 0;
+		map<int, int> mp;
+		mp[0] = 1;
+		for (int i = 0; i < nums.size(); i++)
+		{
+			sum += nums[i];
+			if (mp[sum - k] != 0)
+				result += mp[sum - k];
+			mp[sum]++;
+		}
+		return result;
+	}
+};
 int main()
 {
 	Solution32 s;
